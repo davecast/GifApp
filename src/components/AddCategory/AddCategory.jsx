9b73c1c5015f9cf82cf67939636f3c80254c8cc5
David Castillo <davecas26@gmail.com>
2024-04-15 22:18:15 -0400

    [modified] First component

---
+++ src/components/AddCategory/AddCategory.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const AddCategory = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
+
+export default AddCategory;
